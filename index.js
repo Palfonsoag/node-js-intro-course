7ddfbeaf7a0b7340ab526ff6e368e259425f57c0
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-11-07 22:15:24 -0400

    hello module

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  hello: function () {
+    console.log("Hello World!!!");
+  },
+  helloCustom: function (name) {
+    console.log(`Hello ${name}`);
+  },
+  helloCourse: function () {
+    console.log("Hello npm course");
+  },
+};
